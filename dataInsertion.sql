8d93b7c1cb63699b2b973c84bed050c540b4c96c
ron1502 <56972883+ron1502@users.noreply.github.com>
2019-10-24 12:41:20 -0500

    Update dataInsertion.sql

--- dataInsertion.sql
+++ dataInsertion.sql
@@ -5,16 +5,17 @@ INSERT INTO Customer (email, custName, phoneNo, zipCode, street, city, state, pa
       VALUES('ronk@uak.edu', 'Ronald kingstone', '870-674-9061', '700 N. West 13th Ave.', 'Pine Bluff', 'Arkansas', 'dfsadf3245', 0),
       VALUES('hongKong@hotmail.com', 'Hong Kong', '405-871-3467', '300 S. Creston Drive', 'Oklahoma City', 'Oklahoma', 'fasdf354', 0);
          
- INSERT INTO CreditCard(expDate, name, secCode, number)
-      VALUES('02-22', 'Richard Castro', 340, '8968-4510-8365-9274'),
-      VALUES('05-20', 'Tyler Costello', 890, '5871-8391-5190-2461'),
-      VALUES('12-21', 'Neha Sharma', 120, '7981-7823-9012-7831'),
-      VALUES('09-25', 'Hong Kong', 095, '5721-3719-3749-2910');
+INSERT INTO CreditCard(expDate, name, secCode, number)
+      VALUES('2022-02', 'Richard Castro', 340, '8968-4510-8365-9274'),
+      VALUES('2020-12', 'Tyler Costello', 890, '5871-8391-5190-2461'),
+      VALUES('2021-10', 'Neha Sharma', 120, '7981-7823-9012-7831'),
+      VALUES('2025-09', 'Hong Kong', 095, '5721-3719-3749-2910');
              
- INSERT INTO CustPossesCard(number, accID)
+INSERT INTO CustPossesCard(number, accID)
       VALUES('8968-4510-8365-9274', 1),
       VALUES('5871-8391-5190-2461', 2),
       VALUES('7981-7823-9012-7831', 4),
       VALUES('5721-3719-3749-2910', 5);
   
-      
+INSERT INTO ShoppingCartManage(cName, dateCreated, dateUpdated, accID)
+      VALUES('Dream Books', '2018-03-12', )
