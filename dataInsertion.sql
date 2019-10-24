b8afd3037897fbb8baae1d7d96d0847711304b98
ron1502 <56972883+ron1502@users.noreply.github.com>
2019-10-24 12:11:37 -0500

    Customer Insert added

---
+++ dataInsertion.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customer (email, custName, phoneNo, zipCode, street, city, state, password, rwdPoint)
+  VALUES('richard@gmail.com','Richard Castro', '479-679-9091', 72701, '300 N Garland Ave.', 'Fayetteville', 'Arkansas', 'dfas452', 0),
+  VALUES('costellTyler@yahoo.com', 'Tyler Costello', '501-742-8723', 72002, '300 E Midland Rd.', 'Alexander', 'Arkansas', 'fasdf35', 0),
+  VALUES('neha@outlook.com', 'Neha Sharma', '417-567-0923', 65619, '500 W Glenstone Ave.', 'Springfield, 'Missouri', 'drgt345aw', 0),
+  VALUES('ronk@uak.edu', 'Ronald kingstone', '870-674-9061', '700 N. West 13th Ave.', 'Pine Bluff', 'Arkansas', 'dfsadf3245', 0),
+  VALUES('hongKong@hotmail.com', 'Hong Kong', '405-871-3467', '300 S. Creston Drive', 'Oklahoma City', 'Oklahoma', 'fasdf354', 0);
