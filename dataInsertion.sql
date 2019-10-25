bf5a0c178307b305ef11c9c8cf23106415da5d0a
ron1502 <56972883+ron1502@users.noreply.github.com>
2019-10-25 10:37:49 -0500

    Insertion of some books
    The books inserted and the next ones are gonna be real books. I'm using ISBN 13 instead of 10 and the categories are being used from another online bookstore as references. I'm planning on adding 5 categories and three books per category

--- dataInsertion.sql
+++ dataInsertion.sql
@@ -18,4 +18,18 @@ INSERT INTO CustPossesCard(number, accID)
       VALUES('5721-3719-3749-2910', 5);
   
 INSERT INTO ShoppingCartManage(cName, dateCreated, dateUpdated, accID)
-      VALUES('Dream Books', '2018-03-12', )
+      VALUES('Dream Books', '2018-03-12', '2018-03-13', 1),
+      VALUES('Mom''s Birthday', '2018-03-15', '2018-04-25', 2),
+      VALUES('I''m getting these', '2018-04-26', '2018-04-27', 3),
+      VALUES('New Books!!', '2018-05-12', '2018-07-12', 5),
+      VALUES('Gifts for Chirstmas', '2018-09-30', '2018-10-25', 5);
+
+INSERT INTO Book(category, autName, year, ISBN, title, edition, publisher, stock, avrgRating, price)
+      VALUES('Art & Photography', 'Nikki Sixx', 2001, '0-06-098915-7', 'The Dirt: Confession of the Workd's Most Notorious Rock Band',
+             1, 'HarperEntertainment', 10, 0, 10.00),
+      VALUES('Art & Photography','Stephan Pentak', 2016, '978-1285858227', 'Design Basics', 9, 'CENGAGE Learning',
+             20, 0, 69.99),
+      VALUES('Art & Photography', 'Editors of Phaidon', 2012 , ' 978-0714864679',  'THe Art Book: New Edition', 2,
+             'Phaidon', 15, 0, 70.99),
+      
+             
