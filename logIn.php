796c48e94dcfcfbb90ada554354d17e3e11d7419
ron1502 <ruvelasq@uark.edu>
2019-11-23 16:15:12 -0600

    Connection performed

--- logIn.php
+++ logIn.php
@@ -2,4 +2,15 @@
 	$connData = Array();
 	parse_str(str_replace(";", "&", getenv("MYSQLCONNSTR_localdb")), $connData);
 	print_r($connData);
+	
+
+
+	// Create connection
+	$conn = new mysqli($connData["Data_Source"], $connData["User_id"], $connData["Password"]);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connected successfully";
 ?>
