dfc245b5c2fbbf816ed109ea5951abcd4ca11fd3
ron1502 <ruvelasq@uark.edu>
2019-11-23 14:22:04 -0600

    Fixed

---
+++ sql/dataInsertion.sql
@@ -0,0 +1,89 @@
+USE localdb;
+
+INSERT INTO Customer (email, custName, phoneNo, zipCode, street, city, state, password, rwdPoint)
+    VALUES('richard@gmail.com','Richard Castro', '479-679-9091', 72701, '300 N Garland Ave.', 'Fayetteville', 'Arkansas', MD5('dfas452'), 10),
+		('costellTyler@yahoo.com', 'Tyler Costello', '501-742-8723', 72002, '300 E Midland Rd.', 'Alexander', 'Arkansas', MD5('fasdf35'), 30),
+		('neha@outlook.com', 'Neha Sharma', '417-567-0923', 65619, '500 W Glenstone Ave.', 'Springfield', 'Missouri', MD5('drgt345aw'), 20),
+		('ronk@uak.edu', 'Ronald kingstone', '870-674-9061', 71601, '700 N. West 13th Ave.', 'Pine Bluff', 'Arkansas', MD5('dfsadf3245'), 0),
+		('hongKong@hotmail.com', 'Hong Kong', '405-871-3467', 73008, '300 S. Creston Drive', 'Oklahoma City', 'Oklahoma', MD5('fasdf354'), 0);
+         
+INSERT INTO CreditCard(expDate, name, secCode, number)
+    VALUES('2022-02-01', 'Richard Castro', 340, '8968-4510-8365-9274'),
+		('2020-12-01', 'Tyler Costello', 890, '5871-8391-5190-2461'),
+		('2021-10-01', 'Neha Sharma', 120, '7981-7823-9012-7831'),
+		('2025-09-01', 'Hong Kong', 095, '5721-3719-3749-2910');
+             
+INSERT INTO CustPossesCard(number, accID)
+    VALUES('8968-4510-8365-9274', 1),
+		('5871-8391-5190-2461', 2),
+		('7981-7823-9012-7831', 4),
+		('5721-3719-3749-2910', 5);
+  
+INSERT INTO ShoppingCartManage(cName, dateCreated, dateUpdated, accID)
+    VALUES('Dream Books', '2018-03-12', '2018-03-13', 1),
+    	('Mom''s Birthday', '2018-03-15', '2018-04-25', 2),
+    	('I''m getting these', '2018-04-26', '2018-04-27', 3),
+    	('New Books!!', '2018-05-12', '2018-07-12', 5),
+    	('Gifts for Chirstmas', '2018-09-30', '2018-10-25', 5);
+
+INSERT INTO Book(category, autName, year, ISBN, title, edition, publisher, stock, avrgRating, price)
+    VALUES('Art & Photography', 'Nikki Sixx', 2001, '0-06-098915-7', 'The Dirt: Confession of the Workd''s Most Notorious Rock Band',
+           1, 'HarperEntertainment', 10, 0, 10.00),
+    	('Art & Photography','Stephan Pentak', 2016, '978-1285858227', 'Design Basics', 9, 'CENGAGE Learning',
+           20, 0, 69.99),
+    	('Art & Photography', 'Editors of Phaidon', 2012 , '978-0714864679',  'The Art Book: New Edition', 2,
+           'Phaidon', 15, 0, 70.99),
+    	('Technology', 'Walter Isaacson', 2013, '978-1-4516-4854-6', 'Steve Jobs', 1, 'Norman Seeff', 5, 0, 4.99),
+    	('Technology', 'Mark Lutz', 2009, '978-0-596-15806-4', 'Learning Python: Powerful Object-Oriented Programming',
+          4, 'O''Reilly', 25, 0, 40.99),
+    	('Technology', 'Stephen W. Prate', 1995, '978-1-878739-74-2', 'C++ Primer Plus: Teach Yourself Object-Oriented Programming/Book and Disk',
+          2, 'Waite Group Press', 10, 0, 2.99),
+    	('Health & Fitness', 'Gary Taubes', 2011, '978-0-307-47425-4', 'Why We Get Fat: And What to Do About It',
+           1, 'Alfred A. Knoopf', 10, 0, 6.50),
+    	('Health & Fitness', 'B. K. S. Iyengar', 1995, '978-0-8052-1031-6', 'Light on Yoga: The Bible of Modern Yoga',
+          1, 'Shocken Books', 5, 0, 15.75),
+    	('Health & Fitness', 'Michael Pollan', 2007, '978-0-14-303858-0', 'The Omnivore''s Dilemma : A Natural History of Four Meals',
+          5, 'The Penguin Press', 15, 0, 10.00),
+    	('Science & Math', 'Carl Sagan', 2013, '978-0-345-53943-4', 'Cosmos', 1, 'Ballantine Books', 7, 0, 12.00),
+    	('Science & Math', 'Charles Darwin', 1975, '978-0-393-09219-6', 'The Origin of Species', 1, 'W W Norton & Co Inc',
+           20, 0, 15.99),
+    	('Science & Math', 'Albert Einstein', 1961, '978-0-517-02530-7', 'Relativity: The Special and the General Theory', 1,
+          'Three Rivers Press', 30, 0, 12.99),
+    	('Science Fiction & Fantasy', 'J.K. Rowling', 2009, '978-0-545-13970-0', 'Harry Potter and the Deathly Hallows', 1,
+          'Scholastic Press', 12, 0, 10.50),
+    	('Science Fiction & Fantasy', 'Suzanne Collins', 2012, '978-0-545-42511-7',  'The Hunger Games: Movie Tie-in Edition', 1,
+          'Scholastic Press', 15, 0, 11.00),
+    	('Science Fiction & Fantasy', 'J.R.R. Tolkien', 1997, '978-0-618-00221-4','The Hobbit: or There and Back Again', 1,
+          'Houghton Mifflin Company', 30, 0, 20.30);
+
+INSERT INTO review(title,rating,accID,comment,ISBN)
+VALUES ('Fun', 3.5, 4, 'Enjoyed reading the book','0-06-098915-7'),
+	('Inspiring', 5.0, 3, 'Inspiring story','978-1-4516-4854-6'),
+	('Excellent', 4.5, 2,'Excellent classic read','978-0-618-00221-4'),
+	('Good', 3.8, 1, 'Very interesting, loved every part of it','978-0-345-53943-4');
+
+INSERT INTO cartContBooks(quantity,cartID,ISBN)
+VALUES (2,1,'978-1-4516-4854-6'),
+	(1,2,'978-0-14-303858-0'),
+	(1,2,'978-0-545-13970-0'),
+	(1,3,'978-0-618-00221-4');
+
+INSERT INTO placeOrder(payMethod,billAddr,shipAddr,shipDate,orDate,status,totalPrice,ordID,accID)
+VALUES ('Credit Card','300N Garland Avenue, Apt 12, 72701,Arkansas','300N Garland Avenue, Apt 12, 72701, Arkansas','2019-11-01','2019-10-24','Delivered',65.99,1,1),
+	('Credit Card','300N Garland Avenue, Apt 12, 72701,Arkansas','160 Broadway, 10038, New York','2019-11-20','2019-11-04','Pending to Pay',1280.00,2,1),
+	('Credit Card','700 N. West 13th Ave., Pine Bluff,Arkansas','300N Garland Avenue, Apt 12,72701, Arkansas','2019-11-12','2019-11-06','Ready to Ship',30.68,3,1),
+	('Credit Card','300 S. Creston Drive,Oklahoma City,Oklahoma,73008','300 S. Creston Drive, Oklahoma City, Oklahoma, 73008','2019-11-09','2019-10-30','Shipped',54.66,4,1);
+
+INSERT INTO OrdContBook(quantity,ordID,ISBN)
+VALUES (2,2,'978-1-4516-4854-6'),
+	(1,2,'978-0-14-303858-0'),
+	(1,2,'978-0-545-13970-0'),
+	(1,4,'978-0-618-00221-4');
+
+INSERT INTO employeeMonMan(salary,name,empAddress,rank,ordID)
+VALUES (300,'Steve Young',',72702,Arkansas','Regular',1),
+	(750,'Daisy Johnson','135N Leverett Ave,72703,Arkansas','Manager',2),
+	(350,'Monica Jones','750N Trout Dr,72701,Arkansas','Regular',3),
+	(300,'Cara Sanders','72703,Arkansas','Regular',4),
+	(675,'John Green','72703,Arkansas','Manager', NULL),
+	(350,'Kelly Brooks','72703,Arkansas','Regular', NULL);
---
+++ sql/functions.sql
@@ -0,0 +1,283 @@
+-- SQL statement for the user functions
+-- For Customers
+-- New user account registration
+
+	DROP PROCEDURE IF EXISTS newUser;
+		-- DELITING TEST INSERT
+			DELETE FROM Customer WHERE email = 'randol@uark.edu';
+	DROP PROCEDURE IF EXISTS logIn;
+	DROP PROCEDURE IF EXISTS mergeCart;
+	DROP PROCEDURE IF EXISTS applyRwdPoints;
+	DROP PROCEDURE IF EXISTS cartToOrder;
+
+	DELIMITER $$
+
+	CREATE PROCEDURE newUser(
+		IN email VARCHAR(25),
+		IN custName VARCHAR(20),
+		IN phoneNo VARCHAR(12),
+		IN zipCode MEDIUMINT UNSIGNED,
+		IN street VARCHAR(30),
+		IN city VARCHAR(20),
+		IN state VARCHAR(10),
+		IN password VARCHAR(32),
+		OUT error VARCHAR(2),
+		OUT accID INTEGER UNSIGNED) 
+
+	BEGIN
+		DECLARE hPassword VARCHAR(32);
+		IF EXISTS(SELECT C.email FROM Customer C WHERE C.email = email) THEN
+			SET error = "E";
+			SET accID = 0;
+		ELSE
+			SELECT MD5(password) INTO hPassword;
+			INSERT INTO Customer(email ,custName, phoneNo, zipCode, street, city, state, password)
+			VALUES(email ,custName, phoneNo, zipCode, street, city, state, hPassword);
+			SELECT LAST_INSERT_ID() INTO accID;
+			SET error = "S";
+		END IF;
+	END $$
+	
+	DELIMITER ;
+	
+	-- Test
+	CALL newUser('randol@uark.edu', 'Randol Watson', '212-903-8909', '10001','Park Row', 'New York', 'New York', 'LetsGetThoseBooks', @error, @accID);
+	SELECT @error AS 'Error', @accID AS 'Account ID';
+	
+-- User login
+	DELIMITER $$
+	CREATE PROCEDURE logIn(
+		IN email VARCHAR(25),
+		IN password VARCHAR(32),
+		OUT accID INTEGER UNSIGNED)
+	BEGIN
+		SELECT C.accID FROM Customer C WHERE C.email = email AND C.password = MD5(password) INTO accID;
+	END $$
+	DELIMITER ;
+	
+	-- Test
+	CALL logIn('randol@uark.edu', 'LetsGetThoseBooks', @accID);
+	SELECT @accID AS 'User Account';
+	
+
+-- User can update his address, password etc.
+	
+	UPDATE Customer 
+		SET zipCode = 15004, street = 'South Street', city =  'Atlasburg', state = 'Pennsylvania ',
+		    password = MD5('IJustGotThoseBooks'), phoneNo = '610-333-666'
+		WHERE accID = 6;
+
+-- Book search (by author name, title, category, year or combinations)
+	-- For performance reasons most of the query is going to be implemented in the front end
+	-- WHERE condition is going to be built by putting every aspect of the search together in the front end
+	
+	SELECT ISBN, title FROM Book
+	WHERE autName = 'Stephan Pentak', title = 'Design Basics', category = 'Art & Photography';
+		
+-- Create one empty order or shopping cart
+	-- Shopping Cart
+	INSERT INTO ShoppingCartManage(cName, dateCreated, dateUpdated, accID)
+		VALUES('My New Cart', CONVERT(NOW(), DATE), CONVERT(NOW(), DATE), 1);
+
+	-- Order
+	INSERT INTO placeOrder(payMethod, billAddr, shipAddr, orDate, shipDate, status, totalPrice, accID)
+		VALUES('Credit', '703 W Dickson St', '900 N Leverett Ave.', CONVERT(NOW(), DATE), NULL, 'Pending to Pay', 0.00, 1);
+
+-- Add/delete books from shopping cart
+	-- Adding book
+	INSERT INTO cartContBooks(quantity, cartID, ISBN)
+		VALUES(1, 1, '978-0-596-15806-4');
+        	
+	-- Delete book [Complete deletion of an entry, not individual books]
+	DELETE FROM cartContBooks
+		WHERE ISBN = '978-0-596-15806-4' AND cartID = 1;
+
+-- Merge shopping carts
+	DELIMITER $$
+		CREATE PROCEDURE mergeCart(
+			IN accID INTEGER UNSIGNED,
+			IN name CHAR(20),
+			IN cart1ID INTEGER UNSIGNED, 
+			IN cart2ID INTEGER UNSIGNED, 
+			OUT mergedID INTEGER UNSIGNED)
+		BEGIN
+			INSERT INTO ShoopingCartManage(dateCreate, dateUpdated, accID, cName)
+				VALUES(CONVERT(NOW(), DATE), NULL, accID, name);
+
+			SELECT LAST_INSERT_ID() INTO mergedID;
+
+			UPDATE cartContBooks
+				SET cartID = mergedID
+				WHERE cartID = cart1ID;
+
+			UPDATE cartContBooks
+				SET cartID = mergedID
+				WHERE cartID = cart2ID;
+
+			DELETE FROM ShoppingCartManage
+				WHERE cartID = cart1ID;
+
+			DELETE FROM ShoopingCartManage
+				WHERE cartID = cart2ID;
+		END $$
+
+	DELIMITER ;
+	
+-- Turn shopping carts to orders
+	DELIMITER $$
+
+	CREATE PROCEDURE cartToOrder(
+		IN accID INTEGER UNSIGNED, 
+		IN varCartID INTEGER UNSIGNED, 
+		IN billAddr CHAR(40), 
+		IN payMethod CHAR(12), 
+		IN shipAddr CHAR(40), 
+		OUT varOrdID INTEGER UNSIGNED)
+	BEGIN
+		DECLARE ordDate DATE;
+		DECLARE totalPrice FLOAT;
+
+		SELECT CONVERT(NOW(), DATE) INTO ordDate;
+
+		INSERT INTO placedOrder(payMethod, billAddr, shipAddr, shipDate, orDate, status, accID)
+		VALUES(payMethod, billAddr, shipAddr, NULL, ordDate, 'Pending to Pay', accID);
+
+		SELECT LAST_INSERT_ID() INTO  varOrdID;
+
+		INSERT INTO OrdContBook(ordID, quantity, ISBN)
+			SELECT varOrdID, C.quantity, C.ISBN FROM cartContBooks C
+				WHERE cartID = varCartID;
+
+		SELECT SUM( O.quantity * B.price) 
+			FROM ordContBook O, Book B
+			WHERE O.ordID = varOrdID AND O.ISBN = B.ISBN INTO totalPrice;
+
+		UPDATE placeOrder
+			SET totalPrice = totalPrice
+			WHERE ordID = varOrdID;
+
+		DELETE FROM cartContBooks WHERE cartID =  varCartID;
+		DELETE FROM ShoppingCartManage WHERE cartID = varCartID;
+	END $$
+
+	DELIMITER ;
+
+-- Place the order 
+	UPDATE placeOrder
+		SET shipDate = CONVERT(DATE_ADD(NOW(), INTERVAL 3 DAY, DATE), 
+		status = 'Ready To Ship'
+		WHERE ordID = 24;
+
+-- Order trace 
+	SELECT status FROM placeOrder WHERE ordID = 1;
+
+-- Writ a review
+	INSERT INTO review (title, rating, accID, ISBN, comment)
+		VALUES ('Amazing Book', 3.5, 1, '978-0-14-303858-0', 'The book arrived in excellent conditions');
+	
+-- Edit a written review
+	SELECT comment FROM review 
+		WHERE title = 'Amazing Book' AND accID = 1 AND ISBN = '978-0-14-303858-0';
+	
+	UPDATE review 
+		SET comment = 'This is the new comment'
+		WHERE title = 'Amazing Book' AND accID = 1 AND ISBN = '978-0-14-303858-0';
+
+-- Total reward points 
+	SELECT accID, custName, rwdpoints FROM Customer;
+
+-- Functions for employees:
+-- List book information (e.g., title, author, price) and quantity-in-stock of chosen books
+	SELECT title, author, price, stock FROM Books 
+	WHERE ISBN = '978-0-393-09219-6' OR ISBN = '978-0-517-02530-7';
+
+-- List information about those orders assigned to him/her
+	SELECT * FROM placeorder O, employeeMonMan E 
+		WHERE O.ordID = E.ordID;
+
+-- Insert new books
+	INSERT INTO Books (category, autName , year, ISBN, title, edit, publisher, stock, price)
+		VALUES ( 'Science Fiction & Fantasy', 'George R.R Martin', 1996, '978-1-5247-9628-0',
+			  'Fire and Blood: 300 Years Before A Game of Thrones’, 1, 'Bantam Spectra', 10, 15.98);
+
+-- Update the rating of a book
+	UPDATE book 
+	SET avgRating = (SELECT AVG(r.rating) FROM review r, book b WHERE r.ISBN=b.ISBN);
+
+-- Obtained reward points
+	UPDATE Customer
+		SET rwdPoint = rwdPoint + (SELECT P.totalPrice * 0.1 
+			FROM placeOrder P
+			WHERE P.accID = 1 AND P.orderID = 2)
+		WHERE accID = 1;
+
+-- Applying reward points
+	DELIMITER $$
+	CREATE PROCEDURE applyRwdPoints(
+		IN accID INTEGER UNSIGNED,
+		IN ordID INTEGER UNSIGNED)
+	BEGIN
+		DECLARE rwdPoint FLOAT;
+		DECLARE orderPrice FLOAT;
+
+		SELECT C.rwdPoint FROM Customer C WHERE C.accID = accID INTO rwdPoint;
+		SELECT P.totalPrice FROM placeOrder P WHERE P.ordID = orderID INTO orderPrice;
+
+		IF rwdPoint <= orderPrice THEN
+			SET orderPrice = orderPrice - rwdPoint;
+			SET rwdPoint = 0.0;
+		ELSE
+			SET rwdPoint = rwdPoint - orderPrice;
+			SET orderPrice = 0.0;
+		END IF;
+
+		UPDATE Customer C
+			SET C.rwdPoint = rwdPoint
+			WHERE C.accID = accID;
+
+		UPDATE placeOrder P
+			SET P.totalPrice = orderPrice
+			WHERE P.ordID = ordID;
+	END $$
+	DELIMITER ;
+
+-- Functions for analysts
+-- What types of books are better sold in the second quarter than the first quarter?
+	SELECT b.category, SUM(p.quantity)
+			FROM Book b, OrdContBook o, placeOrder p
+	WHERE b.ISBN=o.ISBN AND p.ordID=o.ordID AND p.orDate
+	BETWEEN('2019-01-01','2019-04-31')
+			GROUP BY b.category
+			ORDER BY SUM(p.quantity)DESC
+			LIMIT 1;
+
+-- What kind of books/customers are most profitable?
+	SELECT b.category, SUM(p.quantity * b.price)
+		FROM Book b, OrdContBook o, placeOrder p
+		WHERE b.ISBN=o.ISBN AND p.ordID=o.ordID 
+		GROUP BY b.category
+		ORDER BY SUM(p.quantity * b.price)DESC
+		LIMIT 1;
+-- What is the average time between the order placed and shipped?
+	SELECT AVG(DATEDIFF(shipDate, orDate))
+		FROM placeOrder
+		WHERE shipDate != NULL AND orDate != NULL;
+
+-- Is there any significant difference between books published by different publishers in terms of profitability? **
+	SELECT b.tittle, b.publisher, SUM(O.quantity * b.price)
+			FROM book b, OrdContBook o ,placeorder p
+			WHERE b.ISBN=o.ISBN AND p.ordID=o.ordID
+			GROUP BY (b.tittle,b.publisher);
+
+-- Does the overall rating effect the sales of a book? *
+	SELECT b.ISBN, b.tittle,  b.avgRating, SUM(o.quantity) FROM book b, OrdContBook o
+		WHERE b.ISBN=o.ISBN GROUP BY (b.ISBN,b.avgRating)
+		ORDER BY b.avgRating; 
+
+-- Who is the most loyal customer (or the top 10 loyal customers) 
+	SELECT accID, custName 
+	FROM Customer 
+	ORDER BY rwdPoint 
+	DESC LIMIT 10;	
+	
+
