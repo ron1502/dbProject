e6c6ea3230e6216bfa26117426a58eda151a2433
ron1502 <ruvelasq@uark.edu>
2019-11-23 15:28:41 -0600

    Test

--- logIn.php
+++ logIn.php
@@ -2,15 +2,18 @@
 	$servername = "localhost";
 	$username = "customer";
 	$password = "N@r@nj@n4r4nj4";
-
+	$dbName = "localdb";
 	// Create connection
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password, $dbName);
 
 	// Check connection
 	if ($conn->connect_error) {
+		$conn->close();
 		die("Connection failed: " . $conn->connect_error);
 	}
+	
 	echo "Connected successfully";
 	$email = $_POST["email"];
 	echo "We got the name:" . $email;
+	$conn->close();
 ?>
