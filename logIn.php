3247d784260134ed982efbea3ec99f55e4bd7bc3
ron1502 <ruvelasq@uark.edu>
2019-11-23 15:49:37 -0600

    Testing dbConnection

--- logIn.php
+++ logIn.php
@@ -1,10 +1,4 @@
 <?php
-$connectionString =  getenv("MYSQLCONNSTR_localdb");
-
+	$connectionString =  getenv("MYSQLCONNSTR_localdb");
 	echo $connectionString;
-	
-	echo "Connected successfully";
-	$email = $_POST["email"];
-	echo "We got the name:" . $email;
-	$conn->close();
 ?>
