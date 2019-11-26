bb911056bfecaeb33586102d1ceed286325d0364
ron1502 <ruvelasq@uark.edu>
2019-11-25 22:11:50 -0600

    Log in request is answered using mutlti query

--- logIn.php
+++ logIn.php
@@ -1,45 +1,60 @@
 <?php
-	$connData = Array();
-	parse_str(str_replace(";", "&", getenv("MYSQLCONNSTR_localdb")), $connData);
-	print_r($connData);
-	
-	// Create connection
-	$db = new mysqli($connData["Data_Source"], $connData["User_Id"], $connData["Password"]);
-	
-	if ($db->connect_error) {
-		die("Connection failed: " . $db->connect_error);
-	}
-	
-	$db->select_db('localdb');
-	
+	$db = getDBConnection();
+	/*
+	$_POST["email"] = "richard@gmail.com";
+	$_POST["password"] = "dfas4";
+	$_POST["funct"] = "logIn";
+	*/
 	if(isset($_POST["funct"])){
 		switch($_POST["funct"]){
 			case "logIn":
-				echo "Login user";
-				echo logInUser();
+				echo logInUser($db);
 				break;
 			case "signIn":
-				echo "SignIn user";
-				echo signInUser();
+				echo signInUser($db);
 				break;
 		}
 	}
+	$db->close();
 	
+	function logInUser($db){
+		$logInQuery = "CALL logIn('". $_POST["email"] . "','" . $_POST["password"] ."', @accID);";
+		$logInQuery .= "SELECT @accID AS accountID;";
+		if($db->multi_query($logInQuery )){
+			$db->next_result(); //Obtaining result from CALL
+			$accID = $db->store_result()->fetch_object()->accountID;
+			if($accID == null) return -1;
+			else return (int) $accID;
+		} else{
+			echo "We ran into problems\n";
+			return -1;
+		}
+	}
 	
-	function logInUser(){
+	function signInUser($db){
 		$id = null;
-		$logInQuery = "logIn(". $_POST["email"] . "," . $_POST["passworrd"]. ");";
-		$logInQuery .= "SELECT @accID AS accountID";
-	
-		$db->multi_query($logInQuery);
-		echo "Selecting from database";
-		$id = $db->store_result()->fetch_object()->accountID;
 		return $id;
 	}
 	
-	function signInUser(){
-		$id = null;
-		return $id;
+	function getDBConnection(){
+		$connData = Array();
+		parse_str(str_replace(";", "&", getenv("MYSQLCONNSTR_localdb")), $connData);
+
+		$db = new mysqli($connData["Data_Source"], $connData["User_Id"], $connData["Password"], "localdb");
+
+		if($db->connect_error) {
+			die("Connection failed: " . $db->connect_error);
+		}
+		return $db;
+	}
+	
+	
+	function getTestDBConnection(){
+		$db = new mysqli("localhost", "root", "", "localdb");
+		if($db->connect_error) {
+			die("Connection failed: " . $db->connect_error);
+		}
+		return $db;
 	}
 	
 ?>
