9a9cfbe42f5883a9728e6af0e75aed16ba7aba6e
ron1502 <ruvelasq@uark.edu>
2019-11-26 13:32:14 -0600

    Connection of DB moved to independent file and it is required in this file

--- logIn.php
+++ logIn.php
@@ -1,4 +1,5 @@
 <?php
+	require "dbConnection.php";
 	$db = getDBConnection();
 	/*
 	$_POST["email"] = "richard@gmail.com";
@@ -38,25 +39,5 @@
 		return $id;
 	}
 	
-	function getDBConnection(){
-		$connData = Array();
-		parse_str(str_replace(";", "&", getenv("MYSQLCONNSTR_localdb")), $connData);
 
-		$db = new mysqli($connData["Data_Source"], $connData["User_Id"], $connData["Password"], "localdb");
-
-		if($db->connect_error) {
-			die("Connection failed: " . $db->connect_error);
-		}
-		return $db;
-	}
-	
-	
-	function getTestDBConnection(){
-		$db = new mysqli("localhost", "root", "", "localdb");
-		if($db->connect_error) {
-			die("Connection failed: " . $db->connect_error);
-		}
-		return $db;
-	}
-	
 ?>
